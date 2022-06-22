444d43e98ec8f0e111d6549ae30b2728b3d72656
Salvador Orjuela <salvadororjuela@gmail.com>
2022-06-21 21:34:07 -0500

    Final corrections for pset1Solution.py to be according to the replit repository

--- py4e/psetsSolutions/pset1Solution1.py
+++ py4e/psetsSolutions/pset1Solution1.py
@@ -5,10 +5,12 @@ def arithmetic_arranger(problems, validator=False):
     result = 0
     moreDigits = 0
     # Elements of the operation to print
-    element1 = list()
-    element2 = list()
-    separationLine = list()
-    total = list()
+    element1 = element2 = separationLine = total = ""
+    # Temporary tuples to store the values that will be converted into str to
+    # be stored in the previous variables (line 8)
+    tupleElement1 = tupleElement2 = sepLineTup = ""
+    tupleTotal = ""
+    separationSpace = "    "
 
     # Verify that the number of operations is no greater than 5 operations
     if len(problems) > 5:
@@ -65,46 +67,81 @@ def arithmetic_arranger(problems, validator=False):
         # of the number to align the element to the right when
         # displaying.
         if len(str(element[0])) < len(str(element[2])):
-            element1 = (" " * (element[-1] - len(str(element[0])) + 1), element[0], "    ")
+            # Store each value in a tuple and then convert all values in
+            # a single str called element1
+            tupleElement1 = " " * \
+                (element[-1] - len(str(element[0]))
+                 ), "  ", element[0], separationSpace
+            for i in tupleElement1:
+                element1 += str(i)
+
         # The opposite case of if the elements 0 and 2 have the same
         # number of digits
         else:
-            element1 = (f"  {element[0]}     ")
+            # Store each value in a tuple and then convert all values in
+            # a single str called element1
+            tupleElement1 = "  ", element[0], separationSpace
+            for i in tupleElement1:
+                element1 += str(i)
 
     for element in results:
         # When the number of digits of the element 0 is greater than
-        # the numberof digits of the element 2, print the operation
+        # the number of digits of the element 2, print the operation
         # symbol (element[1]), and then white spaces ahead of the
         # number to align the element to the right when displaying.
         if len(str(element[0])) < len(str(element[2])):
-            element2 = (f"{element[1]} {element[2]}     ")
+            tupleElement2 = element[1], " ", element[2], separationSpace
+            for i in tupleElement2:
+                element2 += str(i)
         # The opposite case
         elif len(str(element[0])) > len(str(element[2])):
-            element2 = (element[1], " " * (len(str(element[0])) - len(str(element[2]))-1), element[2], "    ")
+            tupleElement2 = element[1], " " * (len(str(element[0])) - len(
+                str(element[2]))-1), "  ", element[2], separationSpace
+            for i in tupleElement2:
+                element2 += str(i)
         # If the elements 0 and 2 have the same number of digits
         else:
-            element2 = (f"{element[1]} {element[2]}     ")
+            tupleElement2 = element[1], " ", element[2], separationSpace
+            for i in tupleElement2:
+                element2 += str(i)
 
-    # Print the divisory line between operation and result addign the
+    # Print the divisory line between operation and result adding the
     # same number  of dashes as the number of digits of the larger
     # number plus two additional dashes.
     for element in results:
-        separationLine = ("-" * (element[-1] + 2))
+        sepLineTup = "-" * (element[-1] + 2), separationSpace
+        for i in sepLineTup:
+            separationLine += i
 
+    # Remove the final separationSpace of each element to avoid errors
+    element1 = element1.rstrip(" ")
+    element2 = element2.rstrip(" ")
+    separationLine = separationLine.rstrip(" ")
+    
     # Determine if the output will display or not the result element of the
     # operation
     if validator is True:
         for element in results:
             # If the result has more digits than the largest element
             if len(str(element[3])) > element[-1]:
-                total = (f" {element[3]}     ")
+                tupleTotal = " ", element[3], separationSpace
+                for i in tupleTotal:
+                    total += str(i)
+            # If the result has less digits than the largest element
+            elif len(str(element[3])) < element[-1]:
+                tupleTotal = "   ", element[3], separationSpace
+                for i in tupleTotal:
+                    total += str(i)
+            # If the result has the same digits the largest element has
             else:
-                total = (f"  {element[3]}     ")
-    
-    print(element1, "\n", element2, "\n", separationLine, "\n", total, "\n")
+                tupleTotal = "  ", element[3], separationSpace
+                for i in tupleTotal:
+                    total += str(i)
+        # Remove the final separationSpace of the total to avoid errors
+        total = total.rstrip(" ")
+        print(element1 + "\n" + element2 + "\n" + separationLine + "\n" + total)
+    else:
+      print(element1 + "\n" + element2 + "\n" + separationLine)
 
-
-# If the arguments include a second argument with value of  "True", display
-# the result. Otherwise, don't print the result
 arithmetic_arranger(['32 - 698', '1 - 3801', '45 + 43',
-                    '123 + 49', '988 + 40'], True)
+                   '123 + 49', '988 + 40'], True)
