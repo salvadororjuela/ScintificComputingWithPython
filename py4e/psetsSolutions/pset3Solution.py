b3db604bd082e032b962bc0d1f5c4bb0e4a0e835
Salvador Orjuela <salvadororjuela@gmail.com>
2022-07-07 16:33:52 -0500

    Methods finished. The next step is to work in the create_spent_chart function

--- py4e/psetsSolutions/pset3Solution.py
+++ py4e/psetsSolutions/pset3Solution.py
@@ -1,7 +1,4 @@
 class Category:
-    # Variable to store movements
-    ledger = list()
-    # Other variables
     categoryName = ""
     amount = 0
     description = ""
@@ -9,40 +6,73 @@ class Category:
     # Method to assign the name of each category when an instance is created
     def __init__(self, nameOfCategory):
         self.categoryName = nameOfCategory
-        print(self.categoryName)
 
     # Method to register the deposits
     def deposit(self, amount, description=""):
-        self.depositAmount = amount
-        self.depositDescription = description
+        # Variable to store movements
+        self.ledger = list()
         # Appends to the ledger() list each deposit as a dictionary
-        Category.ledger.append({"amount": self.depositAmount, "description": self.depositDescription})
+        self.ledger.append({"amount": amount, "description": description})
 
     # Method to register the withdraws
     def withdraw(self, amount, description=""):
         # The nature of the withdraw movements is to subtract, thus the amount is converted to a negative number
-        self.withdrawAmount = amount * -1
-        self.withdrawDescription = description
+        amount = amount * -1
         # Verify if the withdraw movement returns a positive balance, if not don't add it to ledger()
-        validation = Category.verifyPositiveBalance(self, self.withdrawAmount)
+        validation = self.check_funds(amount)
         
         if validation == True:
-            Category.ledger.append({"amount": self.withdrawAmount, "description": self.withdrawDescription})
+            self.ledger.append({"amount": amount, "description": description})
             return True
         else:
             return False
 
-    def get_balance(self, amount):
-        pass
+    def get_balance(self):
+        dictionaries = self.ledger
+        # List to store only the values of each dictionary
+        value = list()
+        values = list()
+        # Store in value() each dictionary value
+        for dictionary in dictionaries:
+            for i in dictionary.values():
+                value.append(i)
 
-    def transfer(self):
-        pass
+        # Select the numeric values and stores them in values()
+        for i in value:
+            if type(i) == int or type(i) == float:
+                values.append(i)
 
-    def check_funds(self):
-        pass
+        return sum(values)
 
-    def verifyPositiveBalance(self, withdraw):
-        dictionaries = Category.ledger
+    def transfer(self, amount, budgetCategory):
+        # Read the budget
+        budget = self.get_balance()
+        # Variable that gets the name of the budget category to where the funds are transfer to
+        transferCategoryName = budgetCategory.categoryName
+        
+        # Determine if there are enough funds in the budget category 
+        if budget - amount < 0:
+            return False
+        # If enough funds, withdraw them from the source category and register in the Category.ledger variable
+        else:
+            # As the amount is a withdraw, it has to be converted into a negative amount
+            amount = amount * -1
+            # Add to the ledger of the origin of the funds the amount and the description
+            self.ledger.append({"amount": amount, "description": f"Tranfer to {transferCategoryName}"})
+
+            # To transfer to the destination budget category, turn the value again in a positive value
+            # because funds are added to this budget category.
+            amount = amount * -1
+            # Add to the ledger of the budget 
+            # catagory where the funds are destinated the value and description of the tranfer
+            budgetCategory.deposit(amount, f"Tranfer from {self.categoryName}")
+            return True
+
+        # Add to the destination ledger the amount and the description 
+        # If not enough funds only return False and don't add funds to any of the 
+
+    def check_funds(self, withdraw):
+        dictionaries = self.ledger
         # List to store only the values of each dictionary
         value = list()
         values = list()
@@ -52,7 +82,7 @@ class Category:
                 value.append(i)
         # Select the numeric values and stores them in values()
         for i in value:
-            if type(i) == int:
+            if type(i) == int or type(i) == float:
                 values.append(i)
         
         # Append the withdraw movement to values()
@@ -62,11 +92,30 @@ class Category:
             return False
         else:
             return True
+        
 
 def create_spend_chart(categories):
     pass
 
 food = Category("Food")
 food.deposit(1000, "initial deposit")
-food.deposit(500, "Second deposit")
-print(food.withdraw(1500.15, "groceries"))
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+print(food.get_balance())
+clothing = Category("Clothing")
+food.transfer(50, clothing)
+print(food.get_balance())
+print(food.ledger)
+print(clothing.ledger)
+clothing.withdraw(25.55)
+print(clothing.get_balance())
+clothing.withdraw(100)
+print(clothing.get_balance())
+auto = Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
+print(auto.ledger)
+print(auto.get_balance())
+
+print(food)
+print(clothing)
--- py4e/psetsSolutions/test.py
+++ py4e/psetsSolutions/test.py
@@ -1,225 +1,122 @@
-def add_time(start, duration, day=None):
-    # Split the components of the start and duration time.
-    start = start.split()
-    duration = duration.split(":")
-    # Split the hours, minutes, and meridiem of start and duration times to store them into variables
-    hourstart = start[0].split(":")
-    hour = int(hourstart[0])
-    minutes = int(hourstart[1])
-    meridiem = start[1]
-    hourduration = int(duration[0])
-    minuteduration = int(duration[1])
-    # Variable to calculate AM or PM in TwoOrMoreDays()
-    hourstart = int(hourstart[0])
-    validator = True
-
-    # Do the addition of time and minutes
-    # This also calculates when the new hour is the am on the same day
-    hour = hour + hourduration
-    minutes = minutes + minuteduration
-
-    # Variable to calculate the number of days later after the time
-    # It uses the round method to determine the am pm meridien.
-    daysLater = round((hour / 24), 2)
-
-    # If the minutes addition is greater than 59, it adds one hour to hour and calculate the remainig 
-    # minutes to display the correct hour and minutes
-    if minutes > 59:
-        minutes = 0 + (minutes - 60)
-        hour = hour + 1
-
-    # Format the minutes to add a 0 before the number if the minutes variable is <= 9
-    if len(str(minutes)) < 2:
-        minutes = f"0{minutes}"
+#                       --> Constructor <--
+class Category:   
     
-    # Determines the meridiem for the same day and up to the next day. 48 hours maximum
-    if meridiem == "PM" and hour > 12 and hour < 24: # Necesary if the start variable is > 12 and meridiem is PM
-        hour = hour - 12
-        meridiem = "AM"
-        new_time = NextDay(hour, minutes, meridiem, day, daysLater)
-        return new_time
-    if meridiem == "PM" and hour < 12: # Necesary if the start variable is < 12 and meridiem is PM
-        hour = hour + 12
-        meridiem = "PM"
-    if hour < 12 or hour >= 24 and hour < 36:
-        meridiem = "AM"
-    if hour >= 12 and hour < 24 or hour >= 36 and hour < 48:
-        meridiem = "PM"
-    
-    # Subtract the number of hours if hours to get the meridiem AM or PM up to the next day.
-    if hour > 12 and hour < 24:
-        hour = hour - 12
-    if hour >= 24 and hour <= 36:
-        hour = hour - 24
-    if hour > 36 and hour < 48:
-        hour = hour -36
-    # Determine the meridiem when the new time of two or more days ahead.
-    if hour >= 48:
-        # Calculate when the new hour is in am or in pm.
-        dayMeridiem = str(daysLater)
-        dayMeridiem = dayMeridiem.split(".")
-
-        # Evaluate if dayMeridiam is starts with a 0 as a string. If so, the meridiem is am
-        if dayMeridiem[1].startswith("0"):
-            meridiem = "AM"
-        # If not, turn dayMeridiem into an integer and determine if the meridiem is am or pm by turning dayMeridiem
-        # into an integer, and if the result is less than 5 then the meridiem is am and viceversa
-        else:
-            dayMeridiem = int(dayMeridiem[1])
-
-            if dayMeridiem < 50:
-                meridiem = "AM"
-            else:
-                meridiem = "PM"
- 
-    # Return the result for the same day
-    if daysLater < 1:
-        new_time = SameDay(hour, minutes, meridiem, day)
-        return new_time
-
-    # Return the result when the meridiem is PM and the second or more days start after 35.59 hours
-    if meridiem == "PM" and hour >= 12 and hour < 24:
-        meridiem = "AM"
-        daysLater = str(daysLater)
-        daysLater = daysLater.split(".")
-        daysLater = int(daysLater[0])
-        daysLater = daysLater + 1
-        dayMeridiem = 75
-        # To avoid miscalculatios dayMeridiem is set to = 75 which is the value when condition in line 80 is met.
-        new_time = TwoOrMoreDays(dayMeridiem, minutes, meridiem, day, daysLater, hourstart, hourduration, validator = False)
-        return new_time
-
-    if meridiem == "PM" and hour >= 24 and hour < 36:
-        meridiem = "PM"
-        new_time = TwoOrMoreDays(dayMeridiem, minutes, meridiem, day, daysLater, hourstart, hourduration, validator)
-        return new_time
-
-    # Return the result if the new time is in the next day
-    if daysLater >= 1 and daysLater < 2:
-        new_time = NextDay(hour, minutes, meridiem, day, daysLater)
-        return new_time
-    
-    # It is necesary to add 1 to daysLater to get the correct count of days that
-    # includes the day in progress. Only necesary for two or more days ahead
-    daysLater = daysLater + 1
-
-    # Call the function to return the result if the new time is more than one day later
-    if daysLater >= 2:
-        new_time = TwoOrMoreDays(dayMeridiem, minutes, meridiem, day, daysLater, hourstart, hourduration, validator)
-        return new_time
-
-
-def SameDay(hour, minutes, meridiem, day):
-    # Return the new hour without the day and viceversa
-    if day == None:
-        new_time = f"{hour}:{minutes} {meridiem}"
-    else:
-        new_time = f"{hour}:{minutes} {meridiem}, {day}"
-
-    return new_time
-
-
-def NextDay(hour, minutes, meridiem, day, daysLater):
-    # Necesary to convert the 0:00 AM into 12:00 AM
-    if hour == 0 and meridiem == "AM":
-        hour = 12
-    # Return the new hour without the day and viceversa
-    if day == None:
-        new_time = f"{hour}:{minutes} {meridiem} (next day)"
-    else:
-        newDay = DaysOfTheWeek(day, daysLater)
-        new_time = f"{hour}:{minutes} {meridiem}, {newDay} (next day)"
-    return new_time
-
-
-def TwoOrMoreDays(dayMeridiem, minutes, meridiem, day, daysLater, hourstart, hourduration, validator):
-    # Calculate the hour in the future
-    hourToCalculate = hourduration % 24
-    newHour = hourToCalculate + hourstart - 12
-    if dayMeridiem == 75 and validator == False:
-        newHour = newHour - 12
-        dayMeridiem = 25
-    # Used to convert the hour when the result is < 0. That means that the meridiem is in the AM frame
-    if newHour <= 0:
-        # + 13 Necesary to include the hour 0, or the newHour will be - 1 hour instead of the real hour
-        newHour = newHour + 13
-    if newHour >= 12:
-        newHour = newHour - 13
-    
-    # Determine the meridiem
-    # If not, turn dayMeridiem into an integer and determine if the meridiem is am or pm by turning dayMeridiem
-    # into an integer, and if the result is less than 5 then the meridiem is am and viceversa. Also determines
-    # the meridiem when the time is PM
-    if meridiem == "PM" and dayMeridiem >= 50:
-        meridiem = "AM"
-    elif meridiem == "PM" and dayMeridiem < 50:
-        meridiem = "PM"
-    elif dayMeridiem <= 1:
-        meridiem = "AM"
-    elif dayMeridiem < 50:
-        meridiem = "AM"
-    else:
-        meridiem = "PM"
-
-    # Get only the int number for daysLater to calculate the index to search
-    daysLater = str(daysLater)
-    daysLater = daysLater.split(".")
-    daysLater = int(daysLater[0])
-
-    # Necesary to convert the 0:00 AM into 12:00 AM
-    if newHour == 0 and meridiem == "AM":
-        newHour = 12
-
-    # Return the new hour without the day and viceversa
-    if day == None:
-        new_time = f"{newHour}:{minutes} {meridiem} ({daysLater} days later)"
-    else:
-        newDay = DaysOfTheWeek(day, daysLater)
-        new_time = f"{newHour}:{minutes} {meridiem}, {newDay} ({daysLater} days later)"
-
-    return new_time
-
-
-# Returns the correct day
-def DaysOfTheWeek(day, daysLater):
-    # Convert the day into camelcase when the new time is one day or more later
-    day = day.title()
-    # Get only the int number for daysLater to calculate the index to search
-    daysLater = str(daysLater)
-    daysLater = daysLater.split(".")
-    daysLater = int(daysLater[0])
-    week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    # Index for the present day
-    dayIndex = week.index(day)
-    # Variable to determine if the next time is a Monday.
-    mondays = (daysLater + dayIndex) % 7
-    
-    # When the next time is in the same week
-    if (dayIndex + daysLater) < 7:
-        day = week[dayIndex + daysLater]
-    # When the next time is exactly a Monday
-    elif mondays == 0:
-        day = week[0]
-    # When the next time is greater than 7 days and it is a day different from Monday
-    else:
-        daysModulo = (daysLater + dayIndex) % 7
-        day = week[daysModulo]
+    def __init__(self, name): # Create the attributes
         
-    return day
+        self.name = name
+        self.ledger = list()
+        self.balance = 0
+        
+    
+    def deposit (self, amount, description=''): #Put a number and sum to the amount
+        self.ledger.append({"amount": amount, "description": description})
+        self.balance += amount
+        
+
+    def withdraw(self, amount, description=''): # Check if possible and subtract the amount
+        if (self.check_funds(amount)):
+            self.ledger.append({"amount": -amount, "description": description})
+            self.balance -= amount
+            return True
+        else:
+            return False
+        
+        
+    def wd_count(self): #Count the withdraw
+        count = 0
+        for entry in self.ledger:
+            if entry["amount"] < 0:
+                count += entry["amount"]
+        return count
 
 
-# print(add_time("3:30 PM", "2:12")) # expected = "5:42 PM"
-# print(add_time("11:55 AM", "3:12")) # expected = "3:07 PM"
-# print(add_time("9:15 PM", "5:30")) # expected = "2:45 AM (next day)"
-# print(add_time("11:40 AM", "0:25")) # expected = "12:05 PM"
-# print(add_time("2:59 AM", "24:00")) # expected = "2:59 AM (next day)"
-# print(add_time("11:59 PM", "24:05")) # expected = "12:04 AM (2 days later)"
-# print(add_time("8:16 PM", "466:02"))  # expected = "6:18 AM (20 days later)" 
-# print(add_time("5:01 AM", "0:00")) # expected = "5:01 AM"
-# print(add_time("3:30 PM", "2:12", "Monday")) # expected = "5:42 PM, Monday"
-# print(add_time("2:59 AM", "24:00", "saturDay")) # expected = "2:59 AM, Sunday (next day)"
-# print(add_time("11:59 PM", "24:05", "Wednesday")) # expected = "12:04 AM, Friday (2 days later)" ####################################
-# print(add_time("9:16 PM", "466:02", "tuesday")) # expected = "7:18 AM, Monday (20 days later)"
+    def get_balance(self): # print the balance (ez)
+        return self.balance
 
 
-# print(add_time("0:00 AM", "49:02"))
+    def transfer(self, amount, category): 
+        if (self.check_funds(amount)):
+            self.withdraw(amount, 'Transfer to ' + category.name)
+            category.deposit(amount, 'Transfer from ' + self.name)
+            return True
+        else:
+            return False
+    
+    
+    def check_funds(self, amount):
+    
+        if amount > self.balance:
+            return False
+        else:
+            return True
+        
+    #                   --> String constructor <--    
+    def __str__(self): 
+            first = f'{self.name:*^30}' + '\n'
+            items = ""
+            for item in self.ledger: #Slicing the description and printing with .2f precision the num.
+                items += f"{item['description'][0:23]:23}" + f"{item['amount']:>7.2f}" + '\n'
+                         
+            return first + items + 'Total: ' + str(self.balance)
+        
+    
+def create_spend_chart(categories): #Expenses chart 
+    title = 'Percentage spent by category'+ '\n'
+    total = 0 #total of expenses
+    counts = {} #dict of category (key) with related expense (value)
+    percentage = {} #percent of each category
+    name_length = 0   
+   
+
+    for category in categories:
+        countkey = category.wd_count() #Grabbing the int value
+        counts[category.name] = countkey #Giving a value to each category
+        total += countkey #Calculating a total of expenses for each category
+
+
+    for name, count in counts.items(): #Get the percent and adding to key
+        percent = count / total * 100
+        percentage[name] = percent
+ 
+    #                   --> Y axis <--
+    x = 100 
+    for number in range(11):
+        row = f"{x}".rjust(3) + "| "
+        for name, percent in percentage.items(): #add the values to the chart
+            if percent >= (x):
+                row += "o  "
+            else:
+                row += "   "
+        title += row + '\n'
+        x -= 10
+   
+    #                   --> X  axis <--
+    x_axis = "    -" 
+    for category in categories:
+        x_axis += ("---")
+    title += x_axis + "\n"
+
+
+    for category in categories: # determines the longest category name length
+        if len(category.name) > name_length:
+            name_length = len(category.name)
+ 
+    
+    y = 0
+    while y <= name_length:
+        rows = "     "
+        for key, value in percentage.items(): # add the category names for x axis values
+            category_name = key
+            try:    
+                rows +=  category_name[y] + "  "
+            except: 
+                rows += "   "
+        
+        if y <= name_length -1:
+            title += rows + '\n' 
+        else:
+            title += rows.strip(" ")
+           
+        y = y + 1
+    title = title.rstrip("\n")
+    return title
