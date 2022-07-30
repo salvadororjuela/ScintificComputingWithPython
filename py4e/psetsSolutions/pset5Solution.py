8b6b83675c43a36cfaaee65c6e6c9142908f7ef8
Salvador Orjuela <salvadororjuela@gmail.com>
2022-07-30 13:49:47 -0500

    pset5 completed. Course finished!!!!!!!!!!!!!!

--- py4e/psetsSolutions/pset5Solution.py
+++ py4e/psetsSolutions/pset5Solution.py
@@ -1,69 +1,79 @@
 import copy
 import random
 from re import findall
-# Consider using the modules imported above.
 
 class Hat:
     # **kwargs is used to accept a variable number of arguments and represent them in a dictionary
     def __init__(self, **kwargs):
-        self.initialContents = list()
-        self.secondaryContents = list()
         self.contents = list()
-        self.kwargs = kwargs
         
         """convert self.copyKwargs into a list of strings that contains the colors times the number 
         of balls of the same color.
         i.e. {blue=4, red=2, green=3} into [blue, blue, blue, blue, red, red, green, green, green]"""
-        for key, value in self.kwargs.items():
-            # Append the color times the number. Result: ['blueblueblue', 'redred', 'greengreengreengreengreengreen']
-            self.initialContents.append(key * value)
+        for key, value in kwargs.items():
+            # Append the color times the number.
+            self.contents += [key] * value
 
-        # It is necessary to read each of the keys in self.kwargs for later use as a regular expresion
-        # in the inner loop to split each element of the self.initialContents and get the following result:
-        # [['blue', 'blue', 'blue', 'blue'], [], [], [], ['red', 'red'], [], [], [], ['green', 'green', 'green', 'green', 'green', 'green']]
-        for i in self.kwargs:
-            for k in self.initialContents:
-                string = findall(i, k)
-                self.secondaryContents.append(string)
-        
-        # Append to the self.contents list only the non empty sub elements in self.secondaryContents.
-        # Result: ['blue', 'blue', 'blue', 'blue', 'red', 'red', 'green', 'green', 'green', 'green', 'green', 'green']
-        for i in self.secondaryContents:
-            for k in i:
-                if k != None:
-                    self.contents.append(k)
-        
-        print(f"Initial contents: {self.contents}")
-            
     def draw(self, qtyDraw):
         drawnBalls = list()
         ball = ""
         length = len(self.contents)
-        # Check that the numbers of ball to draw is less or equal than the quantity of balls in self.contents
-        if qtyDraw > len(self.contents):
-            return False
-        # Else remove randomly the number of balls passed in to the method in the qtyDraw attribute
-        else:
-            # Generate random numbers the number of times passed in
-            for i in range(qtyDraw):
-                # Every number generated will remove the ball in the position corresponding to the random number
-                # from self.contents and store it in the variable drawnBalls.
-                ball = random.randint(0, length - 1) # Randon number. Length resembles a position, it can't have the same amount of the length or it will be out of range, that's why 1 is subtract from length
-                
-                try:
-                    drawnBalls.append(copy.copy(self.contents[ball])) # Append to drawnBalls the element correspondig to the position ball (Random number)
-                except:
-                    print(f"ERROR 'Balls out of range'")
 
-                del self.contents[ball] # Delete the item from self.contents
-                # It is necessary to subtract 1 to len(self.contets) - 1 to avoid an error because the new generated number could be out of range
-                length -= 1
-                
+        if qtyDraw >= length:
+            return self.contents
+        # Remove randomly the number of balls passed in to the method in the qtyDraw attribute
+        # Generate random numbers the number of times passed in
+        for i in range(qtyDraw):
+            # Every number generated will remove the ball in the position corresponding to the random number
+            # from self.contents and store it in the variable drawnBalls.
+            ball = random.randint(0, length - 1) # Randon number. Length resembles a position, it can't have the same amount of the length or it will be out of range, that's why 1 is subtract from length
+            
+            # Append and remove to drawnBalls the element correspondig to the position ball (Random number)
+            drawnBalls.append(self.contents.pop(ball))
+
+            # It is necessary to subtract 1 to len(self.contets) - 1 to avoid an error because the new generated number could be out of range
+            length -= 1
+
         return drawnBalls
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+
+    matches = 0
+
+    # Execute the experiment the num_experimets indicated
+    for i in range(num_experiments):
+        # If match remains true it will add 1 to matches
+        match = True
+        #
+        drawElementsDict = dict()
+        # deepcopy() required for prescision
+        repeatedElements = copy.deepcopy(hat)
+        # Use the experimentDrawnBalls method from the Hat class to get the random balls with the number of balls to draw
+        experimentDrawnBalls = repeatedElements.draw(num_balls_drawn)
+        
+        # Iterates over the drawn balls list of strings returned by the draw method: ["blue", "red", "blue"] and turns the 
+        # values into a dictionary: {"blue": 2, "red":1}
+        for element in experimentDrawnBalls:
+            # Add 1 to each element in the dictionary until the key value pairs are completed
+            values = drawElementsDict.get(element, 0)
+            drawElementsDict[element] = values + 1
+
+        # Validate if the minimum matches are reached comparing expected balls against the drawn balls
+        for element, values in expected_balls.items():
+            # If the values don't match, the loop breaks
+            if drawElementsDict.get(element, 0) < values:
+                match = False
+                break
+        
+        # If match remains True add 1 to the matches counter.
+        if match == True:
+            matches += 1
+
+    # Probability of getting the expected balls in n num_experiments
+    return matches/num_experiments
 
 
-hat = Hat(blue=4, red=2, green=6)
-hat.draw(2)
+hat = Hat(blue=3,red=2,green=6)
+# print(hat.contents)
+# print(hat.draw(3))
+print(experiment(hat=hat, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000))
--- py4e/psetsSolutions/test.py
+++ py4e/psetsSolutions/test.py
@@ -1,191 +1,49 @@
-#                       --> Constructor <--
-class Category:   
-    
-    def __init__(self, name): # Create the attributes
-        
-        self.name = name
-        self.ledger = list()
-        self.balance = 0
-        
-    
-    def deposit (self, amount, description=''): #Put a number and sum to the amount
-        self.ledger.append({"amount": amount, "description": description})
-        self.balance += amount
-        
-
-    def withdraw(self, amount, description=''): # Check if possible and subtract the amount
-        if (self.check_funds(amount)):
-            self.ledger.append({"amount": -amount, "description": description})
-            self.balance -= amount
-            return True
-        else:
-            return False
-        
-        
-    def wd_count(self): #Count the withdraw
-        count = 0
-        for entry in self.ledger:
-            if entry["amount"] < 0:
-                count += entry["amount"]
-        return count
+# Consider using the modules imported above.
+import copy
+import random
 
 
-    def get_balance(self): # print the balance (ez)
-        return self.balance
+class Hat:
+    def __init__(self, **kwargs):
+        self.contents = []
+        for k, v in kwargs.items():
+            self.contents += [k] * int(v)
+
+    def draw(self, num):
+        n = len(self.contents)
+        if num >= n:
+            return self.contents
+        r = []
+        for i in range(0, num):
+            bid = random.randint(0, n - 1)
+            r.append(self.contents.pop(bid))
+            n -= 1
+
+        return r
 
 
-    def transfer(self, amount, category): 
-        if (self.check_funds(amount)):
-            self.withdraw(amount, 'Transfer to ' + category.name)
-            category.deposit(amount, 'Transfer from ' + self.name)
-            return True
-        else:
-            return False
-    
-    
-    def check_funds(self, amount):
-    
-        if amount > self.balance:
-            return False
-        else:
-            return True
-        
-    #                   --> String constructor <--    
-    def __str__(self): 
-            first = f'{self.name:*^30}' + '\n'
-            items = ""
-            for item in self.ledger: #Slicing the description and printing with .2f precision the num.
-                items += f"{item['description'][0:23]:23}" + f"{item['amount']:>7.2f}" + '\n'
-                         
-            return first + items + 'Total: ' + str(self.balance)
-        
-    
-def create_spend_chart(categories): #Expenses chart 
-    title = 'Percentage spent by category'+ '\n'
-    total = 0 #total of expenses
-    counts = {} #dict of category (key) with related expense (value)
-    percentage = {} #percent of each category
-    name_length = 0   
-   
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    count = 0
+    for _ in range(0, num_experiments):
+        h = copy.deepcopy(hat)
+        balls = h.draw(num_balls_drawn)
+        d = {}
+        for b in balls:
+            v = d.get(b, 0)
+            d[b] = v + 1
 
-    for category in categories:
-        countkey = category.wd_count() #Grabbing the int value
-        counts[category.name] = countkey #Giving a value to each category
-        total += countkey #Calculating a total of expenses for each category
+        found = True
+        for b, v in expected_balls.items():
+            if d.get(b, 0) < v:
+                found = False
+                break
+
+        if found:
+            count += 1
+
+    return count / num_experiments
 
 
-    for name, count in counts.items(): #Get the percent and adding to key
-        percent = count / total * 100
-        percentage[name] = percent
- 
-    #                   --> Y axis <--
-    x = 100 
-    for number in range(11):
-        row = f"{x}".rjust(3) + "| "
-        for name, percent in percentage.items(): #add the values to the chart
-            if percent >= (x):
-                row += "o  "
-            else:
-                row += "   "
-        title += row + '\n'
-        x -= 10
-   
-    #                   --> X  axis <--
-    x_axis = "    -" 
-    for category in categories:
-        x_axis += ("---")
-    title += x_axis + "\n"
-
-
-    for category in categories: # determines the longest category name length
-        if len(category.name) > name_length:
-            name_length = len(category.name)
- 
-    
-    y = 0
-    while y <= name_length:
-        rows = "     "
-        for key, value in percentage.items(): # add the category names for x axis values
-            category_name = key
-            try:    
-                rows +=  category_name[y] + "    "
-            except: 
-                rows += "   "
-        
-        if y <= name_length -1:
-            title += rows + '\n' 
-        else:
-            title += rows.strip(" ")
-           
-        y = y + 1
-    title = title.rstrip("\n")
-    return title
-
-
-food = Category("Food")
-entertainment = Category("Entertainment")
-business = Category("Business")
-
-food.deposit(900, "deposit")
-
-food.deposit(45.56)
-
-food.deposit(900, "deposit")
-food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-
-food.deposit(900, "deposit")
-good_withdraw = food.withdraw(45.67)
-
-food.deposit(900, "deposit")
-food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-
-food.deposit(900, "deposit")
-food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-transfer_amount = 20
-print(food.get_balance())
-entertainment_balance_before = entertainment.get_balance()
-food.transfer(transfer_amount, entertainment)
-print(food.get_balance())
-print(entertainment.get_balance())
-
-food.deposit(10, "deposit")
-
-food.deposit(100, "deposit")
-food.withdraw(100.10)
-
-food.deposit(100, "deposit")
-food.transfer(200, entertainment)
-
-food.deposit(900, "deposit")
-food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-food.transfer(20, entertainment)
-
-food.deposit(900, "deposit")
-entertainment.deposit(900, "deposit")
-business.deposit(900, "deposit")
-food.withdraw(105.55)
-entertainment.withdraw(33.40)
-business.withdraw(10.99)
-print(food)
-print(entertainment)
-print(business)
-print(create_spend_chart([business, food, entertainment]))
-
-################################################################
-# food = Category("Food")
-# food.deposit(1000, "initial deposit")
-# food.withdraw(10.15, "groceries")
-# food.withdraw(15.89, "restaurant and more food for dessert")
-# print(food.get_balance())
-# clothing = Category("Clothing")
-# food.transfer(50, clothing)
-# clothing.withdraw(25.55)
-# clothing.withdraw(100)
-# auto = Category("Auto")
-# auto.deposit(1000, "initial deposit")
-# auto.withdraw(15)
-
-# print(food)
-# print(clothing)
-
-# print(create_spend_chart([food, clothing, auto]))
+hat = Hat(blue=3,red=2,green=6)
+print(hat.draw(3))
+print(experiment(hat=hat, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000))
