3696b70fdcda2288b7f08176d35b49d501453b12
Salvador Orjuela <salvadororjuela@gmail.com>
2022-08-20 21:52:24 -0500

    New learning, Numpy and Matplotlib

---
+++ py4e/l18Matplotlib/2pyplotWithPoints.py
@@ -0,0 +1,8 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+xPoints = np.array([1, 8])
+yPoints = np.array([3, 10])
+
+plt.plot(xPoints, yPoints, "o")
+plt.show()
---
+++ py4e/l18Matplotlib/4pyplotDefaultXPoints.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 6, 4, 8, 5, 10])
+
+plt.plot(ypoints)
+plt.show()
